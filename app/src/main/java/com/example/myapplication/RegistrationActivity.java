067e5c2e5b08039c2646656218170fe8094e5e43
ahmedazagaz <contactwithazagaz@gmail.com>
2024-11-16 18:50:12 +0100

    update

--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mFullName;
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnSignup;
+    private TextView mSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,27 @@ public class RegistrationActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        registration();
+    }
+
+
+    private void registration(){
+
+
+        mFullName=findViewById(R.id.FullName_signup);
+        mEmail=findViewById(R.id.Email_signup);
+        mPass=findViewById(R.id.Password_signup);
+        btnSignup=findViewById(R.id.btn_login);
+        mSignin=findViewById(R.id.signin_here);
+
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
