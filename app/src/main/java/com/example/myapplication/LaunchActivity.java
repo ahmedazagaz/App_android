7f7dea3645d59de88587bcd4fb16bbfc014c7ddd
ahmedazagaz <contactwithazagaz@gmail.com>
2024-11-21 14:03:53 +0100

    Done

--- app/src/main/java/com/example/myapplication/LaunchActivity.java
+++ app/src/main/java/com/example/myapplication/LaunchActivity.java
@@ -16,7 +16,7 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
 
         // Apply insets to the main view
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
