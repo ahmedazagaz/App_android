335a3986a4342f4e4f8ac3bea26f95d3747cc8c7
aminbeno <aminebenhadare@gmail.com>
2024-12-11 23:48:14 +0100

    category

--- app/src/main/java/com/example/myapplication/category/Category.java
+++ app/src/main/java/com/example/myapplication/category/Category.java
@@ -1,28 +1,51 @@
 package com.example.myapplication.category;
 
+public class Category {
+    private String id; // ID unique
+    private String name;
+    private int iconResId; // Peut être une URL pour des icônes personnalisées dans Firebase
+    private boolean isMoreButton; // Indique si cette catégorie est un bouton spécial
 
-    public class Category {
-        private String name;
-        private int iconResId;
-        private boolean isMoreButton;
-
-        public Category(String name, int iconResId, boolean isMoreButton) {
-            this.name = name;
-            this.iconResId = iconResId;
-            this.isMoreButton = isMoreButton;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getIconResId() {
-            return iconResId;
-        }
-
-        public boolean isMoreButton() {
-            return isMoreButton;
-        }
+    // Constructeur par défaut requis pour Firebase
+    public Category() {
     }
 
+    public Category( String name, int iconResId, boolean isMoreButton) {
 
+        this.name = name;
+        this.iconResId = iconResId;
+        this.isMoreButton = isMoreButton;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIconResId() {
+        return iconResId;
+    }
+
+    public void setIconResId(int iconResId) {
+        this.iconResId = iconResId;
+    }
+
+    public boolean isMoreButton() {
+        return isMoreButton;
+    }
+
+    public void setMoreButton(boolean moreButton) {
+        isMoreButton = moreButton;
+    }
+}
